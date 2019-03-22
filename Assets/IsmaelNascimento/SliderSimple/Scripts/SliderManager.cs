3eb8acbe5f2feadcfb4a7c7758a9e6772a406849
Ismael Nascimento <contato@ismaelnascimento.com>
2019-03-22 00:50:49 -0300

    initial commit

---
+++ Assets/IsmaelNascimento/SliderSimple/Scripts/SliderManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace IsmaelNascimento
+{
+    public class SliderManager : MonoBehaviour
+    {
+        #region VARIABLES
+
+        [SerializeField] private Button btnPrev;
+        [SerializeField] private Image imgShow;
+        [SerializeField] private Button btnNext;
+        [Space(10)]
+        [SerializeField] private List<Sprite> imagesForShow;
+        private int indexImagesForShow;
+
+        #endregion
+
+        #region METHODS_MONOBEHAVIOUR
+
+        private void Start()
+        {
+            btnPrev.onClick.AddListener(OnButtonPrevClicked);
+            btnNext.onClick.AddListener(OnButtonNextClicked);
+        }
+
+        #endregion
+
+        #region METHODS_PRIVATE
+
+        private void OnButtonPrevClicked()
+        {
+            indexImagesForShow--;
+
+            if (indexImagesForShow < 0)
+            {
+                indexImagesForShow = imagesForShow.Count - 1;
+            }
+
+            imgShow.sprite = imagesForShow[indexImagesForShow];
+        }
+
+        private void OnButtonNextClicked()
+        {
+            indexImagesForShow++;
+
+            if (indexImagesForShow > imagesForShow.Count - 1)
+            {
+                indexImagesForShow = 0;
+            }
+
+            imgShow.sprite = imagesForShow[indexImagesForShow];
+        }
+
+        #endregion
+    }
+}
